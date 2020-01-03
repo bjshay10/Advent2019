678002f0ac2b32950139ee8ae4caaf0c8a32da9f
bjshay10 <bj.shay@d51schools.org>
2020-01-03 13:12:08 -0700

    Advent 2019 Day 1

--- Day1/1_1.js
+++ Day1/1_1.js
@@ -1,1 +1,24 @@
-test
+const lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream('./input.txt')
+})
+
+//total fuel
+var totalFuel = 0
+
+lineReader.on('line', (line) => {
+    //line
+    var l = line
+    //div by 3
+    var div3 = l / 3
+
+    //round down
+    var rndDown = Math.floor(div3)
+
+    //subtract 2
+    var sub2 = rndDown - 2
+
+    // add to total
+    totalFuel = totalFuel + sub2
+
+    console.log(totalFuel)
+})
---
+++ Day1/1_2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const text = fs.readFileSync(path.join(__dirname) + "/input.txt", "utf8");
+let output = text
+  .split("\n")
+  .map(d => calcFuel(d))
+  .reduce((acc, curr) => acc + curr, 0);
+console.log(output);
+
+function calcFuel(mass) {
+  let fuel = calcFuelPerModule(mass);
+  if (fuel > 0) {
+    let fuelFuel = calcFuel(fuel);
+    if (fuelFuel > 0) {
+      fuel += calcFuel(fuel);
+    }
+  }
+  return fuel;
+}
+
+function calcFuelPerModule(mass) {
+  return Math.floor(+mass / 3) - 2;
+}
+
+
